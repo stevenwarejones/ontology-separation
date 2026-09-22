58130a69ac999091a095f5a17d0e4d36802efd1a
Steven Ware Jones <stevenwarejones@gmail.com>
2026-09-21 20:28:09 -0400

    Expose recipe separation adapter

--- OntologySeparation/Recipes.lean
+++ OntologySeparation/Recipes.lean
@@ -1,5 +1,6 @@
 import OntologySeparation.Recipes.TwoQubit
 import OntologySeparation.Recipes.Qubit
+import OntologySeparation.Recipes.Separation
 import OntologySeparation.Reporting.Scenario
 
 /-! One import for the checked recipe workflow and its report command. -/
